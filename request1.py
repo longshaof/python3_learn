e10fd91db771410b21367be500ec1ec8434c9c74
longshaof <fckfirewall@gmail.com>
2018-12-05 22:45:12 +0800

    Create request1.py

---
+++ request1.py
@@ -0,0 +1,13 @@
+import requests
+def getHTMLText(url):
+       try:
+            r=requests.get(url,timeout=30)
+            r.raise_for_status()
+            r.encodeing=r.apparent_encoding
+            return r.txt
+       except:
+            return " 产生异常"
+
+if _name_=="_main_"
+      url="http://www.baidu.com"
+      print(getHTMLTxet(url))
